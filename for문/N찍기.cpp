990e348452e7f66a5736aa3b6ae7f9f3f5bfafa4
youngmin <ymin2570@gmail.com>
2021-02-04 17:49:30 +0900

    Create N찍기.cpp

---
+++ for문/N찍기.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+	int a, sum = 0;
+	cin >> a;
+	for (int i = 1; i <= a; i++)
+	{
+		cout << i << "\n";
+	}
+	return 0;
+}
