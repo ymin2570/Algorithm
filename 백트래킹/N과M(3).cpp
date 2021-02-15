df6a71a39dd1c9d5b8a7ce139c7932ec0c03ebba
youngmin <ymin2570@gmail.com>
2021-02-15 18:03:28 +0900

    Create N과M(3).cpp

---
+++ 백트래킹/N과M(3).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int arr[10] = { 0, };
+int print[10] = { 0, };
+
+void Recursion(int N, int M, int count)
+{
+	for (int i = 0; i < N; i++)
+	{
+		if(print[count] == 0)
+		{
+			print[count] = arr[i];
+			count++;
+			if (M == count)
+			{
+				for (int j = 0; j < M; j++)
+					cout << print[j] << " ";
+				cout << "\n";
+			}
+			if (M > count)
+				Recursion(N, M, count);
+			count--;
+			print[count] = 0;
+		}
+	}
+	return;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+	int N, M, idx = 0;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = i + 1;
+	}
+	Recursion(N, M, idx);
+	return 0;
+}
